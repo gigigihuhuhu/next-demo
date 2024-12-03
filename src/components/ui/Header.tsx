f6c737ae160d7dd52ff8a4f3d98442b8db9e1d7d
“kyungsu” <kyungsu77777@gmail.com>
2024-12-04 00:19:59 +0900

    Add border, Resize Gap for Header

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,7 +3,7 @@ import { Logo, GiftIcon, ProfileIcon, SearchIcon, MessageIcon, AlarmIcon } from 
 
 export default function Header() {
   return (
-    <header className="bg-transparent w-full fixed h-12 py-8 z-10 flex items-center">
+    <header className="bg-transparent w-full fixed h-12 py-8 z-10 flex items-center border-b border-solid border-slate-200">
       <div className="grow flex flex-row justify-between mx-6 max-lg:mx-3 items-center">
         <div>
           <Link className="flex flex-row gap-2 " href="/">
@@ -16,7 +16,7 @@ export default function Header() {
           <Link href="/product/paid">유료</Link>
           <Link href="/product/preference">선호작</Link>
         </div>
-        <div className="flex flex-row gap-8 text-xl font-bold">
+        <div className="flex flex-row gap-4 text-xl font-bold">
           <Link href="/product/free"><SearchIcon></SearchIcon></Link>
           <Link href="/product/top50/free-top"><AlarmIcon></AlarmIcon></Link>
           <Link href="/product/paid"><MessageIcon></MessageIcon></Link>
