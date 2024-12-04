71aee0399b6013b5ba2f0de7456c44682fcf4aa7
“kyungsu” <kyungsu77777@gmail.com>
2024-12-04 22:22:03 +0900

    update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import { AngleNextIcon } from "@/components/Icons";
+
 export default function Home() {
-    return (
-      <>
-        <div className="text-3xl">
-          Not Ready.
-        </div>
-      </>
-    );
-  }
+  const handleClick = () => {
+    window.open("https://github.com/gigigihuhuhu/next-demo");
+  };
+  return (
+    <div className="flex flex-row gap-8 items-center justify-start pl-20">
+      <div className="text-3xl font-semibold">Not yet!</div>
+      <button
+        onClick={handleClick}
+        className="group transition-colors duration-200 text-lg text-gray-900 font-semibold px-4 py-2 border border-solid border-blue-300 rounded-xl hover:border-blue-700 hover:bg-blue-100 flex flex-row items-center"
+      >
+        Go check sourcecode
+        <AngleNextIcon className="transition-transform duration-200 group-hover:translate-x-1"></AngleNextIcon>
+      </button>
+    </div>
+  );
+}
